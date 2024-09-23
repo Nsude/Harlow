d9f15ef9fa4f507cb46dada8d47bf290c9e5f4e3
Meshach Nsude <nsudeme777@gmail.com>
2024-09-23 15:18:22 +0100

    cursor tracker implemented

--- src/components/HomePage/Homepage.tsx
+++ src/components/HomePage/Homepage.tsx
@@ -1,11 +1,7 @@
 import React, { PropsWithChildren } from "react";
 
 const Homepage = () => {
-  return (
-    <div className="homepage-container">
-      
-    </div>
-  );
+  return <div className="homepage-container"></div>;
 };
 
 export default Homepage;

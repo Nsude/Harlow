0602ea26788577e7ffdefbed28bef95748b95df8
Meshach Nsude <nsudeme777@gmail.com>
2024-09-25 09:06:16 +0100

    hero section implemented

--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import placeHolderImg from "../../assets/media/images/navmenu-image (9).webp";
-import { Link } from 'react-router-dom';
-import useCustomEffect from '../../hooks/useCustomEffect';
+import { Link } from "react-router-dom";
+import useCustomEffect from "../../hooks/useCustomEffect";
 import { gsap } from "gsap";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGlobalContext } from "../contexts/GlobalContex";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const HeroSection = () => {
+  const { colors } = useGlobalContext();
   const [time, setTime] = useState("");
 
   useCustomEffect(() => {
@@ -18,11 +20,11 @@ const HeroSection = () => {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit"
-      })
+      });
 
       setTime(NigerianTime);
     }, 500);
-  })
+  });
 
   const imagesDuplicated = useRef(false);
   useCustomEffect(() => {
@@ -37,27 +39,58 @@ const HeroSection = () => {
         imagesDuplicated.current = true;
       }
 
+      // images scroll animation
       const scrollAnimation = gsap.to(".scroller[data-scroll='true']", {
         duration: 40,
         xPercent: -50,
         repeat: -1,
-        ease: "linear",
-      })
+        ease: "linear"
+      });
 
+      let scrollerTimeout: any;
       ScrollTrigger.create({
         trigger: ".scroller",
         start: "top 22%",
-        markers: true,
         end: +500,
-        scrub: 1,
+        scrub: 0.8,
         onUpdate: (self) => {
+          clearTimeout(scrollerTimeout);
           const velocity = self.getVelocity();
-          scrollAnimation.timeScale(1 + velocity / 46);
-        }
+          scrollAnimation.timeScale(1 + velocity / 40);
 
-      })
+          // runs when the user stops scrolling for 200ms
+          scrollerTimeout = setTimeout(() => {
+            scrollAnimation.timeScale(1);
+          }, 200);
+        }
+      });
+
+      // CTA Container
+      gsap.to(".cta-container", {
+        bottom: 0,
+        scrollTrigger: {
+          scrub: 0.2
+        }
+      });
+
+      // bottom info
+      gsap.to(".bottom-info", {
+        opacity: 0,
+        scrollTrigger: {
+          scrub: 0.1
+        }
+      });
+
+      const links = document.querySelector(".cta-container")?.children;
+      if (!links) return;
+      gsap.to(links, {
+        color: colors.black,
+        scrollTrigger: {
+          scrub: 0.2
+        }
+      });
     }
-  }, [])
+  }, []);
 
   return (
     <div className="hero-section-container">
@@ -84,7 +117,7 @@ const HeroSection = () => {
         <p>{time || "00:00:00"}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroSection;
--- src/components/HomePage/Homepage.tsx
+++ src/components/HomePage/Homepage.tsx
@@ -2,9 +2,11 @@ import React, { PropsWithChildren } from "react";
 import HeroSection from "./HeroSection";
 
 const Homepage = () => {
-  return <div className="homepage-container">
-    <HeroSection />
-  </div>;
+  return (
+    <div className="homepage-container">
+      <HeroSection />
+    </div>
+  );
 };
 
 export default Homepage;

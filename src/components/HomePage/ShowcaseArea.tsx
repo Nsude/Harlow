a406eefb6e1c74e22f678f7ff7af0c16b822d13e
Meshach Nsude <nsudeme777@gmail.com>
2024-10-01 16:04:32 +0100

    enlarge tracker when hovering on showcase images

--- src/components/HomePage/ShowcaseArea.tsx
+++ src/components/HomePage/ShowcaseArea.tsx
@@ -3,12 +3,16 @@ import placeHolder from "../../assets/media/images/placeholderImage.png";
 import LinkArrowIcon from "../../assets/icons/LinkArrowIcon";
 import { useGlobalContext } from "../contexts/GlobalContex";
 import useCustomEffect from "../../hooks/useCustomEffect";
+import useMousePos from "../../hooks/useMousePos";
+import { gsap } from "gsap";
+import { getElemByClass } from "../utils";
 
 const ShowcaseArea = () => {
   const { colors } = useGlobalContext();
   const [arrowColor, setArrowColor] = useState(colors.black);
   const buttonsRef = useRef<(HTMLButtonElement | null)[]>([]);
 
+  // button interactions
   useCustomEffect(() => {
     const handleMouseEnter = () => setArrowColor(colors.offWhite);
     const handleMouseLeave = () => setArrowColor(colors.black);
@@ -28,9 +32,31 @@ const ShowcaseArea = () => {
     };
   }, []);
 
+  // image mask
+  const [imageMask, setImageMask] = useState<Element | null>();
+  const mousePos = useMousePos();
+  useCustomEffect(() => {
+    if (!imageMask) return;
+
+    const rect = imageMask.getBoundingClientRect();
+    const relativeX = mousePos.x - rect.left;
+    const relativeY = mousePos.y - rect.top;
+
+    gsap.to(imageMask.firstElementChild, {
+      duration: 0.2,
+      clipPath: `circle(100px at ${relativeX}px ${relativeY}px)`,
+    });
+  }, [mousePos.x, mousePos.y, imageMask]);
+
+  const ImageMaskMouseEnter = (e: React.MouseEvent) => {
+    const target = e.target as HTMLDivElement;
+    setImageMask(target);
+  };
+
   return (
     <div className="showcase-container">
-      <div className="image-container">
+      <div className="image-container" onMouseEnter={(e) => ImageMaskMouseEnter(e)}>
+        <img src={placeHolder} className="image-blur-mask" />
         <div className="content">
           <p>001</p>
           <h2>Freedom to redefine your presence.</h2>
@@ -41,7 +67,8 @@ const ShowcaseArea = () => {
         </div>
         <img src={placeHolder} />
       </div>
-      <div className="image-container">
+      <div className="image-container" onMouseEnter={(e) => ImageMaskMouseEnter(e)}>
+        <img src={placeHolder} className="image-blur-mask" />
         <div className="content">
           <p>002</p>
           <h2>Freedom to redefine your presence.</h2>

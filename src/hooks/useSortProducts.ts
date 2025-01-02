68910c3747fc70393c83923c3a3b9b113802a14e
Meshach Nsude <nsudeme777@gmail.com>
2025-01-02 16:26:13 +0100

    best selling logic

--- src/hooks/useSortProducts.ts
+++ src/hooks/useSortProducts.ts
@@ -12,6 +12,16 @@ import { sortFilters } from "../components/global/ViewProducts";
 export const useSortProducts = (products: Product[], sortValue: string) => {
   const [sorted, setSorted] = useState<Product[] | null>();
 
+  const fisherYatesShuffle = (array: any[]) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+
+    return shuffledArray;
+  }
+
   useCustomEffect(() => {
     if (!sortValue) return setSorted(null);
     const sortProducts = () => {
@@ -19,7 +29,7 @@ export const useSortProducts = (products: Product[], sortValue: string) => {
       switch (sortValue) {
         case sortFilters[0]:
           // best selling
-          sortedProducts = products.sort((a, b) => b.price - a.price);
+          sortedProducts = fisherYatesShuffle(products);
           break;
         case sortFilters[1]:
           // price: High - Low
